ef821024533ec30670a49114b08f547f17737c6f
guhanMurugesan <mguhan.rmd@gmail.com>
2016-06-09 19:15:18 +0530

    intial

---
+++ BaseController.cs
@@ -0,0 +1,26 @@
+﻿using CleanIndia.Handler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CleanIndia.Controllers
+{
+    public class BaseController : Controller
+    {
+        //
+        // GET: /Base/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LiveMap()
+        {
+            LiveTrackHandler handler = new LiveTrackHandler();
+            var detail = handler.getVoteDetail();
+            return View(detail);
+        }
+	}
+}
---
+++ domEvent.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+
+    $('.voteImg').hover(function () {
+        $(this).css('box-shadow', '0px 0px 5px 5px black');
+    }, function () {
+        $(this).css('box-shadow', '0px 0px 0px 0px black');
+    });
+
+    $('.voteImg').click(function () {
+        $('.voteImg').css('box-shadow', '0px 0px 0px 0px black');
+        $(this).css('box-shadow', '0px 0px 5px 5px black');
+    });
+
+    $('#goLive').click(function () {
+        window.location.href = liveMapUrl;
+    });
+
+    $('#userMap').click(function () {
+        window.location.href = userMapUrl;
+    });
+
+    $('#voteNotify').click(function () {
+        $(this).hide();
+        $('#votePoll').animate({ left: '0%' }, 1000);
+
+    });
+});
